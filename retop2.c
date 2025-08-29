b4b10fc81d27482b5c831ea7c3e04703a96dbc40
SantiTGS <santiagocarvajal46@gmail.com>
2025-08-29 09:59:18 -0500

    Add files via upload

--- retop2.c
+++ retop2.c
@@ -2,24 +2,24 @@
 #include <stdlib.h>
 
 int main() {
-    int n, i;
-    int *p;
+    long n, i;
+    long *p;
 
     scanf("%d", &n);
 
-    // Reservamos memoria para n enteros
+ 
     p = (int *)malloc(n * sizeof(int));
     if (p == NULL) {
         printf("Error al asignar memoria\n");
         return 1;
     }
 
-    // Leer los elementos
+   
     for (i = 0; i < n; i++) {
         scanf("%d", p + i);   
     }
 
-    // Imprimir en orden inverso usando punteros
+
     for (i = n - 1; i >= 0; i--) {
         printf("%d ", *(p + i));
     }
